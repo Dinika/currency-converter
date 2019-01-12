0a2cf51caafd1433600a2b59cecf16deaaf912b4
Dinika <dinikasaxenas@gmail.com>
2019-01-12 20:41:11 +0530

    currencyDetailForm: add prop-type validation
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/components/CurrencyDetailForm/CurrencyDetailForm.jsx
+++ src/components/CurrencyDetailForm/CurrencyDetailForm.jsx
@@ -1,26 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form } from 'semantic-ui-react';
+import PropTypes from 'prop-types';
 
-const CurrencyDetailForm = (props) => {
-    return (
-        <Form>
-            <Form.Group widths='equal'>
-                <Form.Field
-                    placeholder={props.currencyAmount}
-                    control='input' 
-                    value={props.currencyAmount}
-                    name={props.name} 
-                    type="number"
-                    onChange={props.onCurrencyAmountChange} 
-                />
-                <Form.Field control='select' onChange={props.onCurrencyChange}>
-                    {
-                        props.currencyOptions.map(currency => <option key={currency} value={currency}>{currency}</option>)
-                    }
-                </Form.Field>
-            </Form.Group>
-        </Form>
-    )
+class CurrencyDetailForm extends Component {
+    renderCurrencyOptions() {
+        return(
+            this.props.currencyOptions.map(currency => (
+                <option 
+                    key={currency} 
+                    value={currency}>{currency}
+                </option>)
+            )
+        )
+    }
+
+    render() {
+        return (
+            <Form>
+                <Form.Group widths='equal'>
+                    <Form.Field
+                        placeholder={this.props.currencyAmount}
+                        control='input' 
+                        value={this.props.currencyAmount}
+                        name={this.props.name} 
+                        type="number"
+                        onChange={this.props.onCurrencyAmountChange} 
+                    />
+                    <Form.Field 
+                        name={this.props.name} 
+                        control='select'
+                        value={this.props.selectedValue}
+                        onChange={this.props.onCurrencyTypeChange}>
+                        {this.renderCurrencyOptions()}
+                    </Form.Field>
+                </Form.Group>
+            </Form>
+        )
+    }
 }
+
+CurrencyDetailForm.propTypes = {
+   currencyOptions: PropTypes.arrayOf(PropTypes.string).isRequired,
+   currencyAmount: PropTypes.number.isRequired,
+   onCurrencyAmountChange: PropTypes.func.isRequired,
+   onCurrencyTypeChange: PropTypes.func.isRequired,
+};
 
 export default CurrencyDetailForm;
