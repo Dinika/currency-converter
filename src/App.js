ff8ce30751d45f25160be507b9ddae120df4babb
Dinika <dinikasaxenas@gmail.com>
2019-01-13 14:12:03 +0530

    global: add eslint and prettier
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import Layout from './components/Layout/Layout';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Layout />
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <Layout />
+  </div>
+);
 
 export default App;
